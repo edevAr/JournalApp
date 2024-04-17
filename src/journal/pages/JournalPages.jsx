66d28e247b4fe8b114c4a9e4c981c58daccaa1e8
Eden Guzman Andia <eden.guzman@niceincontact.com>
2024-04-17 15:34:49 -0400

    Adding more views to the app

--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -1,12 +1,27 @@
-import { MailOutline } from '@mui/icons-material'
-import { Typography } from '@mui/material'
+import { AddOutlined, MailOutline } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPages = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPages</Typography>
-      <MailOutline/>
-    </>
+    <JournalLayout>
+      <NoteView/>
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{fontSize: 30}}/>
+      </IconButton>
+    </JournalLayout>
   )
 }
